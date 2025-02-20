b4f39b749166659a0597e0f4225148f3c53437ef
stefanbinoj <stefan.binoj.007@gmail.com>
2025-02-20 23:08:50 +0530

    fix

--- Backend/config/passport-config.js
+++ Backend/config/passport-config.js
@@ -39,7 +39,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:4002/api/auth/google/callback",
+      callbackURL: "http://localhost:4002/api/users/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
